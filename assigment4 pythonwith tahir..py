280dda8727bb133ff721d8e70004389ecd807ff3
nasir ibrahim abba <ibrahimnasir618@gmail.com>
2019-12-23 03:54:24 -0800

    assigment4 pythonwithtahir

---
+++ assigment4 pythonwith tahir..py
@@ -0,0 +1,20 @@
+def numbers():
+    numbers =[8,4]
+    
+    print(max(numbers))
+    print(min(numbers))
+    
+ 
+def my_max(list):
+    num1=(input("enter your first number:"))
+    num2 =(input(f"enter your second number:\n"))
+    print(f"this is the max of the num1:{max(num1)}")
+    print(f"this is the min of the num1:{min(num1)}\n")
+     
+     
+    print(f"this is the max of the num2:{max(num2)}")
+    print(f"this is the min of the num2:{min(num2)}")
+    
+numbers()
+my_max()
+
